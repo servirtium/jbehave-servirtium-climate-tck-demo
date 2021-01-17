8629f9740cd66fc7c78884a33c0803b3181fab35
Paul Hammant <paul@hammant.org>
2021-01-17 16:10:44 +0000

    kill JFrame

--- src/main/java/dev/servirtium/jbehave/climate/ClimateStories.java
+++ src/main/java/dev/servirtium/jbehave/climate/ClimateStories.java
@@ -3,9 +3,6 @@ package dev.servirtium.jbehave.climate;
 import org.jbehave.core.Embeddable;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
-import org.jbehave.core.context.Context;
-import org.jbehave.core.context.ContextView;
-import org.jbehave.core.context.JFrameContextView;
 import org.jbehave.core.embedder.PropertyBasedEmbedderControls;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.StoryFinder;
@@ -13,14 +10,10 @@ import org.jbehave.core.junit.JUnitStories;
 import org.jbehave.core.model.ExamplesTableFactory;
 import org.jbehave.core.model.TableTransformers;
 import org.jbehave.core.parsers.RegexStoryParser;
-import org.jbehave.core.reporters.ContextOutput;
-import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.reporters.SurefireReporter;
-import org.jbehave.core.steps.ContextStepMonitor;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
-import org.jbehave.core.steps.NullStepMonitor;
 
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
@@ -34,11 +27,6 @@ public class ClimateStories extends JUnitStories {
 
     public static final String CLIMATEDATA_URL = "http://climatedataapi.worldbank.org";
     public static final String MD_PATH = "src/main/resources/mocks";
-
-    protected final Context context = new ClimateContext();
-    private Format contextFormat = new ContextOutput(context);
-    private ContextView contextView = new JFrameContextView().sized(640, 120);
-    private ContextStepMonitor contextStepMonitor = new ContextStepMonitor(context, contextView, new NullStepMonitor());
 
     public ClimateStories() {
         configuredEmbedder().embedderControls().doGenerateViewAfterStories(true).doIgnoreFailureInStories(false)
@@ -62,13 +50,12 @@ public class ClimateStories extends JUnitStories {
                 .withNamingStrategy(new SurefireReporter.BreadcrumbNamingStrategy()).doReportByStory(true);
         SurefireReporter surefireReporter = new SurefireReporter(embeddableClass, options);
         return new MostUsefulConfiguration()
-                .useStepMonitor(contextStepMonitor)
                 .useStoryLoader(resourceLoader)
                 .useStoryParser(new RegexStoryParser(examplesTableFactory))
                 .useStoryReporterBuilder(
                         new StoryReporterBuilder()
                                 .withCodeLocation(codeLocationFromClass(embeddableClass))
-                                .withFormats(contextFormat, ANSI_CONSOLE)
+                                .withFormats(ANSI_CONSOLE)
                                 .withFailureTrace(true)
                                 .withFailureTraceCompression(true)
                                 .withSurefireReporter(surefireReporter))
